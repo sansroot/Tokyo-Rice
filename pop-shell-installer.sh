20d69e317a1322a26ac269f586b0f1c4b78275b5
sansroot <kuringasan@outlook.com>
2023-04-07 00:55:17 -0300

    Tokyo Rice V1

---
+++ pop-shell-installer.sh
@@ -0,0 +1,4 @@
+sudo apt install git node-typescript make
+git clone https://github.com/pop-os/shell.git
+cd shell
+make local-install
---
+++ theme-installer.sh
@@ -0,0 +1,2 @@
+sudo mv ./sources/Tokyo-Rice -t /usr/share/themes/
+
